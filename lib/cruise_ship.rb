c41eeef9d0fba84d975aa25c58427b69fe06a8aa
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-31 15:29:01 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,10 +8,9 @@
 
 def select_winner(passengers)
   winner = ""
+
   passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
+    if suite = "A" && name.start_with("A")
       winner = name
     end
   end
-  winner
-end
